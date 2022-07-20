8214d8317c61344b487e7520abb274dad893adac
Zachary Stiers <zstiers@protonmail.com>
2022-07-19 22:45:27 -0700

    First attempt to duplicate standalone with a new name. Planning to make it scriptable later.

---
+++ lessons/ch1_setup/include/Ch1/Ch1Dialect.h
@@ -0,0 +1,16 @@
+//===- Ch1Dialect.h - Ch1 dialect -----------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef STANDALONE_STANDALONEDIALECT_H
+#define STANDALONE_STANDALONEDIALECT_H
+
+#include "mlir/IR/Dialect.h"
+
+#include "Ch1/Ch1OpsDialect.h.inc"
+
+#endif // STANDALONE_STANDALONEDIALECT_H
