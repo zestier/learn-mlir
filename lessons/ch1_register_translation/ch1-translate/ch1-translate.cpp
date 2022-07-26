5f8dff95829ef9096f34a2cafb87c50bfdde6b1e
Zachary Stiers <zstiers@protonmail.com>
2022-07-25 22:18:59 -0700

    Registered a custom translation. Does nothing, but is registered for future use.

--- lessons/ch1_register_translation/ch1-translate/ch1-translate.cpp
+++ lessons/ch1_register_translation/ch1-translate/ch1-translate.cpp
@@ -11,17 +11,50 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "llvm/Support/SourceMgr.h"
 #include "mlir/InitAllTranslations.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/MLIRContext.h"
 #include "mlir/Support/LogicalResult.h"
 #include "mlir/Tools/mlir-translate/MlirTranslateMain.h"
+#include "mlir/Tools/mlir-translate/Translation.h"
 
 #include "Ch1/Ch1Dialect.h"
 
-int main(int argc, char **argv) {
-  mlir::registerAllTranslations();
+namespace {
+  mlir::OwningOpRef<mlir::ModuleOp> translateToModule(
+      llvm::SourceMgr&    sourceMgr,
+      mlir::MLIRContext*  context
+  ) {
+    auto& memoryBuffer = *sourceMgr.getMemoryBuffer(sourceMgr.getMainFileID());
+    auto module = mlir::ModuleOp::create(
+      mlir::FileLineColLoc::get(
+        context,
+        memoryBuffer.getBufferIdentifier(),
+        /*line=*/0,
+        /*column=*/0
+      )
+    );
 
-  // TODO: Register ch1 translations here.
+    return module;
+  }
+
+  void registerFromCh1Translation() {
+    mlir::TranslateToMLIRRegistration fromCh1(
+      "import-ch1", [](llvm::SourceMgr& sourceMgr, mlir::MLIRContext* context) {
+        return translateToModule(sourceMgr, context);
+      }
+    );
+  }
+}
+
+int main(int argc, char** argv) {
+  mlir::registerAllTranslations();
+  registerFromCh1Translation();
 
   return failed(
-      mlir::mlirTranslateMain(argc, argv, "MLIR Translation Testing Tool"));
+      mlir::mlirTranslateMain(argc, argv, "ch1-translate")
+  );
 }
