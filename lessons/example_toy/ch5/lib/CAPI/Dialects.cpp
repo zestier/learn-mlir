ff820a5ba034cd9ad0661eb5963e5dd5f4b457e3
Zachary Stiers <zstiers@protonmail.com>
2022-12-14 23:36:41 -0800

    Addition of Toy Ch5

---
+++ lessons/example_toy/ch5/lib/CAPI/Dialects.cpp
@@ -0,0 +1,15 @@
+//===- Dialects.cpp - CAPI for dialects -----------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "toy-c/Dialects.h"
+
+#include "toy/Ch5ToyDialect.h"
+#include "mlir/CAPI/Registration.h"
+
+MLIR_DEFINE_CAPI_DIALECT_REGISTRATION(Toy, toy,
+                                      mlir::toy::ToyDialect)
